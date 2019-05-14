c2de22b4349c23d88a094f5d1edc79c323b5002d
Robin Westhaver <robinwesthaver@gmail.com>
2019-05-14 22:21:02 +0000

    used jquery to update character counter and added red CSS styling if negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.new-tweet form textarea').on('input', function(event) {
+        const length = $(this).val().length;
+        const originalCounter = $(this).parent().find('.counter');
+        const count = 140 - length; 
+        originalCounter.text(count);
+
+        //add class for red CSS when under 0
+        (count < 0) ? originalCounter.addClass("red") : originalCounter.removeClass("red");
+    });
+  });
