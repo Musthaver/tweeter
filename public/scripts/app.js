40dea4b2ec45ce9a013c8e457135cdb335d889d3
Robin Westhaver <robinwesthaver@gmail.com>
2019-05-15 19:07:29 +0000

    added ajax POST and GET

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,59 +5,9 @@
  */
 
 
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
-
-
 function renderTweets(tweets) {
     for (const tweet of tweets) {
         const $tweet = createTweetElement(tweet);
-        console.log($tweet);
         $('#tweets').append($tweet);
     }
 } 
@@ -93,6 +43,56 @@ const createTweetElement = (tweetObj) => {
     return $article;
 };
 
+
 $(function() {
-renderTweets(data);
+// renderTweets(data);
+
+$('.new-tweet form').on('submit', function(event) {
+  // stopping the form to being submitted
+  event.preventDefault();
+  $.ajax({
+    method: 'POST',
+    url: "/tweets",
+    data: $(this).serialize()
+  })
+    // callback function when the request is done. We have access to the response.
+    .done(response => {
+      // Creating and adding all the posts to the page
+      console.log($(this).serialize());
+    })
+    // Catching an error with the request
+    .fail(error => {
+      console.log(`Error: ${error}`);
+    })
+    // This will always execute
+    .always(() => {
+      console.log('Request completed.');
+    });
+ 
+}) 
+
+function loadtweets (url) {
+  console.log(url);
+  $.ajax({
+    method: 'GET',
+    url: url,
+  })
+    // callback function when the request is done. We have access to the response.
+    .done(response => {
+      console.log(response);
+      // Creating and adding all the posts to the page
+      renderTweets(response);
+    })
+    // Catching an error with the request
+    .fail(error => {
+      console.log(`Error: ${error}`);
+    })
+    // This will always execute
+    .always(() => {
+      console.log('Request completed.');
+    });
+};
+
+loadtweets("http://localhost:8080/tweets");
+
 });
